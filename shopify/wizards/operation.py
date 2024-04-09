9ef7e43f710dcff44bdde6a1cb50ae28bb16814e
hspl-parth <parva@heliconia.in>
2024-04-09 18:51:45 +0530

    [UPDATE]: 9th April :- Product fetching completed.Order fetching done only state and tax is remaining

--- shopify/wizards/operation.py
+++ shopify/wizards/operation.py
@@ -109,6 +109,8 @@ class operationImport(models.TransientModel):
                     raise e
 
     def get_attribute(self, attribute_name):
+        '''Function to return the attribute id if exist or creates the new attribute'''
+
         attribute_id = self.env['product.attribute'].search([('name', '=', attribute_name)], limit=1)
         if not attribute_id:
             attribute_id = self.env['product.attribute'].create({
@@ -120,6 +122,8 @@ class operationImport(models.TransientModel):
         return attribute_id.id
 
     def get_attribute_values(self, attribute_id, values):
+        '''Function to return the values of corresponding attibute id if exist or creates the new value for provided attribute'''
+
         value_ids = []
         for value in values:
             value_id = self.env['product.attribute.value'].search(
@@ -152,11 +156,24 @@ class operationImport(models.TransientModel):
                         else:
                             tag_list.append(tag_id.id)
 
-                product_exist = productenv.search([('shopify_product_id', '=', str(product["id"]))])
+                product_values = {
+                    'shopify_product_id': str(product["id"]),
+                    'name': product["title"],
+                    'detailed_type': 'product',
+                    'product_tag_ids': [(6, 0, tag_list)],
+                }
+
+                print('product_values.....', product_values)
+
+                product_id = productenv.search([('shopify_product_id', '=', str(product["id"]))])
+                if not product_id:
+                    product_id = self.env['product.template'].create(product_values)
+                    print('Template Created')
+                else:
+                    product_id.write(product_values)
+                    print('Template Updated')
 
                 variable_list = []
-                # attribute_list = []
-                # value_list = []
                 variants = product.get('options')
 
                 if variants:
@@ -164,53 +181,124 @@ class operationImport(models.TransientModel):
                         attribute_id = self.get_attribute(variant.get('name'))
 
                         values = variant.get('values')
-                        value_list = self.get_attribute_values(attribute_id,values)
+                        value_list = self.get_attribute_values(attribute_id, values)
 
-                        attribute_line = {}
-                        attribute_line.update({'attribute_id': attribute_id, 'value_ids': [(6, 0, value_list)]})
-                        print('attribute_line', attribute_line)
+                        if attribute_id:
+                            if value_list:
+                                attribute_line = self.env['product.template.attribute.line'].search(
+                                    [('attribute_id', '=', attribute_id),
+                                     ('value_ids', 'in', value_list),
+                                     ('product_tmpl_id', '=', product_id.id)], limit=1)
+                                if not attribute_line:
+                                    attribute_line = self.env['product.template.attribute.line'].create({
+                                        'attribute_id': attribute_id,
+                                        'value_ids': [(6, 0, value_list)],
+                                        'product_tmpl_id': product_id.id
+                                    })
+                                else:
+                                    attribute_line.write({
+                                        'attribute_id': attribute_id,
+                                        'value_ids': [(6, 0, value_list)],
+                                        'product_tmpl_id': product_id.id
+                                    })
+                                print('attribute_line===', attribute_line)
 
-                        if product_exist:
-                            variable_list.append((6, 0, attribute_line))
-                        else:
-                            variable_list.append((0, 0, attribute_line))
+                product_variant = self.env['product.product'].search(
+                    [('product_tmpl_id', '=', product_id.id)])
 
-                        print('variable_list', variable_list)
+                variants = product.get('variants')
+                if variants:
+                    for variant in variants:
+                        options = []
+                        if variant.get('option1') == 'Default Title' and product_id:
+                            product_id.sudo().write({
+                                'list_price': variant.get('price'),
+                                'weight': variant.get('weight'),
+                            })
 
+                        if variant.get('option1'):
+                            options.append(variant.get('option1'))
+                        if variant.get('option2'):
+                            options.append(variant.get('option2'))
+                        if variant.get('option3'):
+                            options.append(variant.get('option3'))
+                        print('options//////', options)
 
-                # for pro in products:
-                product_values = {
-                    'shopify_product_id': str(product["id"]),
-                    'name': product["title"],
-                    'product_tag_ids': [(6, 0, tag_list)],
-                    'attribute_line_ids': variable_list
-                }
+                        for item in product_variant:
+                            if item.product_template_attribute_value_ids:
+                                print('-----item', item)
+                                print('item.product_template_attribute_value_ids',
+                                      item.product_template_attribute_value_ids)
+                                list_values = []
+                                for rec in item.product_template_attribute_value_ids:
+                                    list_values.append(rec.name)
+                                print('list_values---->', list_values)
 
-                print("Valueeeeeees", product_values)
-
-                # product_exist = productenv.search([('shopify_product_id', '=', str(product["id"]))])
-                print("product_exist", product_exist)
-                try:
-                    if product_exist:
-                        product_exist.write(product_values)
-                        print("Product Updated")
-                    else:
-                        productenv.create(product_values)
-                        print("Product   Created")
-                except Exception as e:
-                    raise e
-
-        print("Product printed")
+                                if set(options).issubset(set(list_values)):
+                                    item.shopify_variant_id = variant.get('id')
+                                    item.shopify_product_id = variant.get('product_id')
+                                    item.barcode = variant.get('barcode')
+                                    item.weight = variant.get('weight')
+                                    item.lst_price = variant.get('price')
 
     def update_orders(self, response_data):
 
-        # for rec in self:
-        #     saleorder = self.env['sale.order']
-        #     list_orders = response_data.get("orders")
-        #
-        #     for order in list_orders:
-        #         values = {
-        #             'shopify_orders_id': str(order.get("id")),
-        #         }
+        for rec in self:
+            orderenv = self.env['sale.order']
+
+            orders = response_data.get('orders')
+            for order in orders:
+
+                customer_shopify_id = str(order.get('customer').get('id'))
+                customer_id = self.env['res.partner'].search([('shopify_customer_id', '=', str(customer_shopify_id))])
+
+                billing_name = order.get('billing_address').get('name')
+                billing_id = self.env['res.partner'].search([('name', '=', billing_name)], limit=1)
+
+                shipping_name = order.get('shipping_address').get('name')
+                shipping_id = self.env['res.partner'].search([('name', '=', shipping_name)], limit=1)
+
+                values = {
+                    'shopify_orders_id': order.get('id'),
+                    'partner_id': customer_id.id,
+                    'partner_invoice_id': billing_id.id,
+                    'partner_shipping_id': shipping_id.id,
+                }
+
+                order_id = orderenv.search([('shopify_orders_id', '=', str(order.get('id')))])
+                if not order_id:
+                    order_id = orderenv.create(values)
+                else:
+                    order_id.write(values)
+
+                line_items = order.get('line_items')
+                for line_item in line_items:
+                    variant_id = line_item.get('variant_id')
+
+                    product_variant = self.env['product.product'].search([('shopify_variant_id', '=', str(variant_id) )])
+                    product_variant_id = product_variant.id
+                    product = product_variant.shopify_product_id
+
+                    for tax in line_item.get('tax_lines'):
+                        tax_title = tax.get('title')
+                        tax_rate = (tax.get('rate')) * 100
+
+                    order_line_values = {
+                        'order_id': order_id.id,
+                        'product_id': product_variant_id,
+                        'product_uom_qty': line_item.get('quantity'),
+                        'discount': float(line_item.get('total_discount')) * 100,
+                    }
+
+                    order_line = self.env['sale.order.line'].search([
+                        ('order_id', '=', order_id.id),
+                        ('product_id', '=', product_variant_id),
+                        ('product_uom_qty', '=', line_item.get('quantity')),
+                        ('discount', '=', float(line_item.get('total_discount')) * 100),
+                    ])
+                    if not order_line:
+                        order_line = self.env['sale.order.line'].create(order_line_values)
+                    else:
+                        order_line.write(order_line_values)
 
         print("Orders printed")
